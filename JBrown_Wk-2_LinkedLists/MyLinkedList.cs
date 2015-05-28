483c21f0afb448d8faf12880bd6d0afe3c3bac65
Jason <jasbrown@uat.edu>
2015-05-27 17:12:49 -0700

    Still working on the SortList() method.

--- JBrown_Wk-2_LinkedLists/MyLinkedList.cs
+++ JBrown_Wk-2_LinkedLists/MyLinkedList.cs
@@ -1,13 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* CSC382 - Data Structures & Algorithms
+ * 18 MAY, 2015 - Week 2
+ * Jason Brown
+ * 
+ * Linked List - Using code from: http://channel9.msdn.com/Forums/TechOff/A-simple-and-pure-C-implementation-of-the-good-old-linked-list
+ * Written by: Mauricio Feijo
+ * Added find minimum and find maximum (and minor changes)
+ * 
+ * Implementation
+ * Implement a templated doubly-linked list in C++.  Your linked list class should implement the following functionality:
+ * 
+ * Find(dataToFind) – returns a pointer to the node whose key matches dataToFind.
+ * Insert(newData) – inserts a new node at the end of the list with a key equal to newData
+ * Delete(delNode)- deletes the node that delNode points to from the list.
+ * Minimum() – returns the minimum key in the list
+ * Maximum() –returns the maximum key in the list
+ * 
+ * Your main program should instantiate, initialize and correctly demonstrate the functionality of the linked list.
+ * 
+ * Lab Report
+ * In Word or a similar word processing program, write a Lab Report including the following sections:
+ * Introduction – in your own words, describe the structure and operations of linked lists.
+ * Programmer’s Guide  – Provide detailed documentation describing your implementation of linked lists. Describe the structure of the list class (class diagrams are awesome), and describe the meaning and operation of each of its member variables and methods. For each member function, give a usage example.
+ * 
+ * Submission
+ * Submit a *.zip file containing your lab report and code project folder to the drop-box associated with this assignment.  Name the *.zip file with your Last Name, and the week.  For example, my submission file would be:
+ * 
+ * GordonWeek2.zip
+ */
+
+// Only need System!
+using System;
 
 namespace DblLinkedList
 {
     public class MyLinkedList
     {
+        /// <summary>
+        /// Basic Node.
+        /// <params>
+        /// int NodeContent, Node Next, Node Prior
+        /// </params>
+        /// </summary>
         public class Node
         {
             public int NodeContent;
@@ -20,7 +53,7 @@ namespace DblLinkedList
         {
             get
             {
-                return size;
+                return (size - 1);
             }
         }
 
@@ -63,15 +96,16 @@ namespace DblLinkedList
             else
             {
                 current.Next = newNode;
+                newNode.Prior = current;
             }
 
             current = newNode;
         }
 
         /// <summary>
-        /// Displays all nodes currently in the list.
+        /// Vertically displays all nodes currently in the list.
         /// </summary>
-        public void DisplayNodes()
+        public void DisplayNodesVertical()
         {
             Node tempNode = head;
 
@@ -80,6 +114,29 @@ namespace DblLinkedList
                 Console.WriteLine(tempNode.NodeContent);
                 tempNode = tempNode.Next;
             }
+        }
+
+        /// <summary>
+        /// Horizontally displays all nodes currently in the list.
+        /// </summary>
+        public void DisplayNodesHorizontal()
+        {
+            Node tempNode = head;
+
+            while (tempNode != null)
+            {
+                if (tempNode.Next == null)
+                {
+                    Console.Write(tempNode.NodeContent.ToString());
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(tempNode.NodeContent + ", ");
+                }
+                tempNode = tempNode.Next;
+            }
+            Console.WriteLine();
         }
 
         /// <summary>
@@ -146,10 +203,10 @@ namespace DblLinkedList
 
             while (tempNode != null)
             {
-                if (tempNode.Next == null)
-                {
-                    break;
-                }
+                //if (tempNode.Next == null)
+                //{
+                //    break;
+                //}
 
                 if (minNode.NodeContent > tempNode.NodeContent)
                 {
@@ -186,9 +243,51 @@ namespace DblLinkedList
             return minNode.NodeContent;
         }
 
+        /// <summary>
+        /// Reverses the horizontal display of all nodes currently in the list.
+        /// </summary>
+        public void DisplayNodesReverseHorizontal()
+        {
+            Node tempNode = this.FindNode(Count);
 
+            for (int counter = this.Count; counter > 0; counter--)
+            {
+                if (counter == 1)
+                {
+                    Console.Write(this.FindNode(counter).NodeContent);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(this.FindNode(counter).NodeContent + ", ");
+                }
+            }
+        }
 
-
+        /// <summary>
+        /// Sorts the list in ascending order.
+        /// </summary>
+        public void SortList()
+        {            
+            //Node tempNode = head;
+            //Node nextNode = tempNode.Next;
+            //int tempContent = tempNode.NodeContent;
+                        
+            //while (nextNode != null)
+            //{
+            //    if (tempNode.NodeContent < nextNode.NodeContent && nextNode != null)
+            //    {
+            //        tempNode = tempNode.Next;
+            //    }
+            //    else
+            //    {
+            //        tempContent = tempNode.Next.NodeContent;
+            //        tempNode.Next.NodeContent = tempNode.NodeContent;
+            //        tempNode.NodeContent = tempContent;
+            //        tempNode = tempNode.Next;
+            //    }
+            //}
+        }
     }
 }
  
--- JBrown_Wk-2_LinkedLists/Program.cs
+++ JBrown_Wk-2_LinkedLists/Program.cs
@@ -4,26 +4,8 @@
  * 
  * Linked List
  * 
- * Implementation
- * Implement a templated doubly-linked list in C++.  Your linked list class should implement the following functionality:
+ * Driver program to demonstrate features.
  * 
- * Find(dataToFind) – returns a pointer to the node whose key matches dataToFind.
- * Insert(newData) – inserts a new node at the end of the list with a key equal to newData
- * Delete(delNode)- deletes the node that delNode points to from the list.
- * Minimum() – returns the minimum key in the list
- * Maximum() –returns the maximum key in the list
- * 
- * Your main program should instantiate, initialize and correctly demonstrate the functionality of the linked list.
- * 
- * Lab Report
- * In Word or a similar word processing program, write a Lab Report including the following sections:
- * Introduction – in your own words, describe the structure and operations of linked lists.
- * Programmer’s Guide  – Provide detailed documentation describing your implementation of linked lists. Describe the structure of the list class (class diagrams are awesome), and describe the meaning and operation of each of its member variables and methods. For each member function, give a usage example.
- * 
- * Submission
- * Submit a *.zip file containing your lab report and code project folder to the drop-box associated with this assignment.  Name the *.zip file with your Last Name, and the week.  For example, my submission file would be:
- * 
- * GordonWeek2.zip
  */
 
 using System;
@@ -32,8 +14,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-using Aesthetics; // Custom class make console output more readable
 
 namespace DblLinkedList
 {
@@ -45,47 +25,83 @@ namespace DblLinkedList
             int nodeContent = 0;
             int initNumNodes = 15; // Total nodes to generate randomly
             int ceilingNum = 100; // Maximum value for any given node
+            int floorNum = 12; // Maximum value for any given node
 
-            SpacerClass s = new SpacerClass('+');
+            // SpacerClass s = new SpacerClass('+');
+            // 's' object allows for a spacer bar, header, and a clear screen method
+            TempAesthetics s = new TempAesthetics();
+            s.ShowSpacer();
 
+            // Create a new linked list - null at the moment
             MyLinkedList firstList = new MyLinkedList();
 
+            // Populate the list with pseudo random numbers
             for (int count = 0; count < initNumNodes; count++)
             {
-                nodeContent = randNum.Next(1,ceilingNum);
+                nodeContent = randNum.Next(floorNum, ceilingNum);
                 firstList.Insert(nodeContent);
             }
 
+            // Shows a title at the top
+            s.DisplayHeader();
+
             Console.WriteLine("> Display the randomly generated nodes: ");
-            firstList.DisplayNodes();
-            s.showSpacer();
+            s.ShowSpacer();
+            firstList.DisplayNodesVertical();
+            s.ShowSpacer();
+            s.ClearScreen();
 
             Console.WriteLine("> Inserting the value '11' at the end: ");
+            s.ShowSpacer();
             firstList.Insert(11);
-            firstList.DisplayNodes();
-            s.showSpacer();
+            firstList.DisplayNodesHorizontal();
+            s.ShowSpacer();
+            s.ClearScreen();
 
             Console.WriteLine("> Inserting the value '99' at the end: ");
+            s.ShowSpacer();
             firstList.Insert(99);
-            firstList.DisplayNodes();
-            s.showSpacer();
+            firstList.DisplayNodesHorizontal();
+            s.ShowSpacer();
+            s.ClearScreen();
 
             Console.WriteLine("> Node inserted at index 17 (last node) is now: {0}", firstList.FindNode(17).NodeContent);
-            s.showSpacer();
+            s.ShowSpacer();
+            firstList.DisplayNodesHorizontal();
+            s.ClearScreen();
 
             Console.WriteLine("> Deleting Node at index 17: ");
             firstList.DeleteNode(17);
-            firstList.DisplayNodes();
-            s.showSpacer();
+            s.ShowSpacer();
+            firstList.DisplayNodesHorizontal();
+            s.ShowSpacer();
+            s.ClearScreen();
 
+            firstList.DisplayNodesHorizontal();
+
+            s.ShowSpacer();
             Console.WriteLine("> The value at node 2 is: {0}", firstList.FindNode(2).NodeContent);
-            s.showSpacer();
+            s.ShowSpacer();
 
             Console.WriteLine("> The minimum value is: {0}", firstList.MinimumNode());
-            s.showSpacer();
+            s.ShowSpacer();
 
             Console.WriteLine("> The maximum value is: {0}", firstList.MaximumNode());
-            s.showSpacer();
+            s.ShowSpacer();
+
+            Console.WriteLine("> The size of the list is: {0}", firstList.Count);
+            s.ShowSpacer();
+
+            Console.WriteLine("> The last node of the list is: {0}", firstList.FindNode(firstList.Count).NodeContent);
+            s.ShowSpacer();
+
+            firstList.DisplayNodesReverseHorizontal();
+            s.ShowSpacer();
+
+            firstList.SortList();
+
+            firstList.DisplayNodesReverseHorizontal();
+            s.ShowSpacer();
             
         }
 
